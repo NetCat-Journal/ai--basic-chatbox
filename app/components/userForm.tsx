1dec0c2a25468bcd81ab53f5e3e77d8875bb2cd2
Magda78 <magda.78@hotmail.com>
2025-07-01 22:16:53 -0400

    debug

--- app/components/userForm.tsx
+++ app/components/userForm.tsx
@@ -75,7 +75,7 @@ function UserForm() {
             </div>
             <form
                 onSubmit={handleSubmit}
-                className="mt-4 w-full border border-gray-300 rounded-2xl px-4 py-2 bg-[#1a1035] pb-[env(safe-area-inset-bottom)]"
+                className="mt-4 w-full border border-gray-300 rounded-2xl px-4 py-2 bg-[#1a1035] border-t"
             >
                 <div className="flex flex-row">
                     <input
